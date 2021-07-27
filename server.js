4150935693cdf2527c66885f8df4c86d308f6ccd
DeltaSAMP <47721109+DeltaSAMP@users.noreply.github.com>
2021-07-27 11:12:54 +0200

    insert nice shit

--- server.js
+++ server.js
@@ -31,3 +31,11 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get("/api/whoami?", (req, res) => {
+  res.json({
+    ipadress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  })
+})
